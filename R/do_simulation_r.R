984edd6f4b4ee70c36ca97471b8daa9addba4a81
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-07 17:48:57 +0100

    This may work

--- R/do_simulation_r.R
+++ R/do_simulation_r.R
@@ -4,21 +4,7 @@
 #' @export
 do_simulation_r <- function(csv_filename)
 {
-
-  exe_filename <- paste(Sys.getenv("HOME"), "/GitHubs/experimentr/bin/main", sep = "")
-  #csv_filename <- paste(Sys.getenv("HOME"), "/GitHubs/experimentr/bin/results.csv", sep = "")
-  if (!file.exists(exe_filename))
-  {
-    stop("File '", exe_filename, "'not found")
-  }
-  cmd <- paste(exe_filename, " ", csv_filename, sep = "")
-  system(cmd)
-
-  if (!file.exists(csv_filename))
-  {
-    stop("File '", csv_filename, "'not found")
-  }
-
+  do_simulation(csv_filename)
   result <- read.csv(csv_filename, header = TRUE, sep = ",")
   result
 }
